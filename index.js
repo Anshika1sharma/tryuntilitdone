0b07e1769f46dd69591c8f74f6c745b5334ddf29
Anshika Sharma <140472829+Anshika1sharma@users.noreply.github.com>
2025-08-25 22:56:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var len = document.getElementsByClassName("drum").length;
+
+for (var i = 0; i < len; i++) {
+  document.getElementsByClassName("drum")[i]
+    .addEventListener("click", handleProgram);
+}
+document.addEventListener("keydown", (event) => {
+  handleProgramKeys(event.key);
+  buttonAnimation(event.key);
+});
+
+function handleProgram() {
+  let drumClass = this.innerHTML;
+
+  switch (drumClass) {
+    case 'w': new Audio("./sounds/tom-1.mp3").play(); break;
+    case 'a': new Audio("./sounds/tom-2.mp3").play(); break;
+    case 's': new Audio("./sounds/tom-3.mp3").play(); break;
+    case 'd': new Audio("./sounds/tom-4.mp3").play(); break;
+    case 'j': new Audio("./sounds/crash.mp3").play(); break;
+    case 'k': new Audio("./sounds/kick-bass.mp3").play(); break;
+    case 'l': new Audio("./sounds/snare.mp3").play(); break;
+  }
+}
+
+function handleProgramKeys(keynote) {
+  switch (keynote) {
+    case 'w': new Audio("./sounds/tom-1.mp3").play(); break;
+    case 'a': new Audio("./sounds/tom-2.mp3").play(); break;
+    case 's': new Audio("./sounds/tom-3.mp3").play(); break;
+    case 'd': new Audio("./sounds/tom-4.mp3").play(); break;
+    case 'j': new Audio("./sounds/crash.mp3").play(); break;
+    case 'k': new Audio("./sounds/kick-bass.mp3").play(); break;
+    case 'l': new Audio("./sounds/snare.mp3").play(); break;
+  }
+}
+
+function buttonAnimation(key) {
+  let activeButton = document.querySelector("." + key); 
+
+  if (activeButton) { 
+    activeButton.classList.add("animation");
+
+    setTimeout(() => {
+      activeButton.classList.remove("animation");
+    }, 100); 
+  }
+}
+
+
+
