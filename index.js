9644f0deec9b92c7d711c5dd8985279659708c41
Anshika Sharma <140472829+Anshika1sharma@users.noreply.github.com>
2025-08-25 22:59:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,25 +13,25 @@ function handleProgram() {
   let drumClass = this.innerHTML;
 
   switch (drumClass) {
-    case 'w': new Audio("./sounds/tom-1.mp3").play(); break;
-    case 'a': new Audio("./sounds/tom-2.mp3").play(); break;
-    case 's': new Audio("./sounds/tom-3.mp3").play(); break;
-    case 'd': new Audio("./sounds/tom-4.mp3").play(); break;
-    case 'j': new Audio("./sounds/crash.mp3").play(); break;
-    case 'k': new Audio("./sounds/kick-bass.mp3").play(); break;
-    case 'l': new Audio("./sounds/snare.mp3").play(); break;
+    case 'w': new Audio("./tom-1.mp3").play(); break;
+    case 'a': new Audio("./tom-2.mp3").play(); break;
+    case 's': new Audio("./tom-3.mp3").play(); break;
+    case 'd': new Audio("./tom-4.mp3").play(); break;
+    case 'j': new Audio("./crash.mp3").play(); break;
+    case 'k': new Audio("./kick-bass.mp3").play(); break;
+    case 'l': new Audio("./snare.mp3").play(); break;
   }
 }
 
 function handleProgramKeys(keynote) {
   switch (keynote) {
-    case 'w': new Audio("./sounds/tom-1.mp3").play(); break;
-    case 'a': new Audio("./sounds/tom-2.mp3").play(); break;
-    case 's': new Audio("./sounds/tom-3.mp3").play(); break;
-    case 'd': new Audio("./sounds/tom-4.mp3").play(); break;
-    case 'j': new Audio("./sounds/crash.mp3").play(); break;
-    case 'k': new Audio("./sounds/kick-bass.mp3").play(); break;
-    case 'l': new Audio("./sounds/snare.mp3").play(); break;
+    case 'w': new Audio("./tom-1.mp3").play(); break;
+    case 'a': new Audio("./tom-2.mp3").play(); break;
+    case 's': new Audio("./tom-3.mp3").play(); break;
+    case 'd': new Audio("./tom-4.mp3").play(); break;
+    case 'j': new Audio("./crash.mp3").play(); break;
+    case 'k': new Audio("./kick-bass.mp3").play(); break;
+    case 'l': new Audio("./snare.mp3").play(); break;
   }
 }
 
@@ -46,6 +46,7 @@ function buttonAnimation(key) {
     }, 100); 
   }
 }
+
 
 
 
